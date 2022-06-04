9daf42434b3bea24b581a883b23c7ee960cb1b09
rmsmartins <52045144+rmsmartins@users.noreply.github.com>
2022-06-04 01:56:06 +0100

    change language

--- src/main/java/myBookings.java
+++ src/main/java/myBookings.java
@@ -2,12 +2,32 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class myBookings {
 
 	@Test
-	public void viewHotelVoucher() {
+	public void changeLanguage() throws Exception {
+		
+		System.setProperty("webdriver.chrome.driver", "D:/Projetos/Drivers/chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.phptravels.net/login");
+		System.out.println(driver.getTitle());
+		Assert.assertEquals("Login - PHPTRAVELS", driver.getTitle());
+		
+		driver.findElement(By.id("languages")).click();
+		driver.findElement(By.xpath("//a[@href='https://www.phptravels.net/lang-en']")).click();
+		Thread.sleep(1000);
+		driver.findElement(By.xpath("//a[contains(text(),'Login') and @class='theme-btn theme-btn-small theme-btn-transparent ml-1 waves-effect']")).click();
+		System.out.println();
+
+	}
+	
+	
+	@Test
+	public void viewHotelVoucher() throws Exception {
 		
 		System.setProperty("webdriver.chrome.driver", "D:/Projetos/Drivers/chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
@@ -17,6 +37,12 @@ public class myBookings {
 		Assert.assertEquals("Login - PHPTRAVELS", driver.getTitle());
 		
 
+		driver.findElement(By.id("languages")).click();
+		driver.findElement(By.xpath("//a[@href='https://www.phptravels.net/lang-en']")).click();
+		Thread.sleep(1000);
+		driver.findElement(By.xpath("//a[contains(text(),'Login') and @class='theme-btn theme-btn-small theme-btn-transparent ml-1 waves-effect']")).click();
+		System.out.println("Clicou no login");
+		
 		driver.findElement(By.name("email")).sendKeys("user@phptravels.com");
 		driver.findElement(By.name("password")).sendKeys("demouser");
 
